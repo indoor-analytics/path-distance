d23bc5cfb18ae7f21d565613a04f338495ce85d0
Alystrasz <raes.remy@gmail.com>
2021-03-02 11:34:58 +0100

    [test] pathDistance should throw if some input paths are empty

--- test/distance.test.ts
+++ test/distance.test.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import {path1, path2} from "./test.features";
 import {pathDistance} from "../src/distance/distance";
+import {lineString} from "@turf/helpers";
 
 describe ('distance', () => {
     it ('should return some vectors', () => {
@@ -16,5 +17,23 @@ describe ('distance', () => {
             expect(vector.index).to.equal(index);
             index += 1;
         }
+    });
+
+    it ('should throw with first path being empty', () => {
+        expect(() => {
+            pathDistance(lineString([]), path2);
+        }).to.throw(Error, 'coordinates must be an array of two or more positions');
+    });
+
+    it ('should throw with second path being empty', () => {
+        expect(() => {
+            pathDistance(path2, lineString([]));
+        }).to.throw(Error, 'coordinates must be an array of two or more positions');
+    });
+
+    it ('should throw with both paths having no coordinates', () => {
+        expect(() => {
+            pathDistance(lineString([]), lineString([]));
+        }).to.throw(Error, 'coordinates must be an array of two or more positions');
     });
 });
