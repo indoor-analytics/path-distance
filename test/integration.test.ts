182e3c00384c2bd13b163b860b36d49402064017
Alystrasz <raes.remy@gmail.com>
2021-03-02 16:54:47 +0100

    [test] adding integration test

---
+++ test/integration.test.ts
@@ -0,0 +1,20 @@
+import {pathDistance} from "../src/main";
+import {path1, path2} from "./test.features";
+import {lineString, FeatureCollection, featureCollection} from "@turf/helpers";
+import { expect } from "chai";
+import * as fs from "fs";
+
+function printCollectionToFile (collection: FeatureCollection): void {
+    fs.writeFileSync('node.json', JSON.stringify(collection));
+}
+
+// You can use http://geojson.io/ to visualize data exported in node.json
+describe ('Integration test', () => {
+    it ('should produce some vectors', () => {
+        const vectors = pathDistance(path1, path2);
+        const vectorsLines = vectors.map((vector) =>
+            lineString([vector.projectedPoint, vector.acquiredPoint], {"stroke": "#ff0000"}));
+        printCollectionToFile(featureCollection([path1, path2, ...vectorsLines]));
+        expect(vectorsLines.length).to.equal(path2.geometry!.coordinates.length);
+    });
+});
