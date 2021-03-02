9f78710c9d73539a308e60c554508f53267a8f10
Alystrasz <raes.remy@gmail.com>
2021-03-02 16:08:09 +0100

    [feat] adding createVectorFrom helper method

--- src/distance/distance.ts
+++ src/distance/distance.ts
@@ -1,6 +1,6 @@
-import {Feature, LineString} from "@turf/helpers";
+import {Feature, LineString, Point} from "@turf/helpers";
 import ErrorVector from "./ErrorVector";
-import {lineDistance} from "@turf/turf";
+import {distance, lineDistance} from "@turf/turf";
 
 /**
  * Compares a path to a reference path by projecting its positions on the reference path.
@@ -30,3 +30,27 @@ function getLineDistanceDelta (
     return lineDistance(referencePath) / lineDistance(acquiredPath);
 }
 
+
+/**
+ * Computes the distance between an acquired position and its projection, and creates an error vector from it.
+ *
+ * @param pos1 acquired position
+ * @param pos2 projected position
+ * @param index place of the acquired point in its path
+ * @param coveredDistance distance from the projected position to the origin of the reference path
+ * @private
+ */
+function createVectorFrom (
+    pos1: Feature<Point>,
+    pos2: Feature<Point>,
+    index: number,
+    coveredDistance: number
+): ErrorVector {
+    return new ErrorVector(
+        index,
+        pos1.geometry!.coordinates,
+        pos2.geometry!.coordinates,
+        distance(pos1, pos2, {units: 'meters'}),
+        coveredDistance*1000
+    );
+}
