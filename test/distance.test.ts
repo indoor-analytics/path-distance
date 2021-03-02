5db8e0a89a8212fe813b4b36cf21a94c877eb80b
Alystrasz <raes.remy@gmail.com>
2021-03-02 11:27:31 +0100

    [test] pathDistance should return vectors in correct order

--- test/distance.test.ts
+++ test/distance.test.ts
@@ -7,4 +7,14 @@ describe ('distance', () => {
         const vectors = pathDistance(path1, path2);
         expect(vectors.length).to.not.equal(0);
     });
+
+    it ('should return vectors in correct order', () => {
+        const vectors = pathDistance(path1, path2);
+        let index = 0;
+
+        for (const vector of vectors) {
+            expect(vector.index).to.equal(index);
+            index += 1;
+        }
+    });
 });
