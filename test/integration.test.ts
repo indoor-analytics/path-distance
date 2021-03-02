793de9b8928244a747e16d9963f0c67ff9ed7e83
Alystrasz <raes.remy@gmail.com>
2021-03-02 18:31:07 +0100

    [test] using method from NPM package

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,8 +1,9 @@
-import {pathDistance} from "../main";
 import {path1, path2} from "./test.features";
 import {lineString, FeatureCollection, featureCollection} from "@turf/helpers";
 import { expect } from "chai";
 import * as fs from "fs";
+import * as imported from "@indoor-analytics/path-distance";
+import {pathDistance} from "../main";
 
 function printCollectionToFile (collection: FeatureCollection): void {
     fs.writeFileSync('node.json', JSON.stringify(collection));
@@ -15,6 +16,13 @@ describe ('Integration test', () => {
         const vectorsLines = vectors.map((vector) =>
             lineString([vector.projectedPoint, vector.acquiredPoint], {"stroke": "#ff0000"}));
         printCollectionToFile(featureCollection([path1, path2, ...vectorsLines]));
+        expect(vectorsLines.length).to.equal(path2.geometry!.coordinates.length);
+    });
+
+    it ('should be able to use exposed method from NPM package', () => {
+        const vectors = imported.pathDistance(path1, path2);
+        const vectorsLines = vectors.map((vector) =>
+            lineString([vector.projectedPoint, vector.acquiredPoint], {"stroke": "#ff0000"}));
         expect(vectorsLines.length).to.equal(path2.geometry!.coordinates.length);
     });
 });
