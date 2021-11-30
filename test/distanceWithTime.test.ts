f3377b6514cac2c3b01b72c793d36127edc76185
Remy Raes <raes.remy@gmail.com>
2021-11-30 11:11:18 +0100

    [test] distanceWithTime should throw if timestamps array length does not match checkpoints length

--- test/distanceWithTime.test.ts
+++ test/distanceWithTime.test.ts
@@ -7,4 +7,9 @@ describe('distanceWithTime', () => {
         const getDistance = () => distanceWithTime(path1, path2, []);
         expect(getDistance).to.throw(RangeError, 'Timestamps array must not be empty.');
     });
+
+    it ('should throw if timestamps array length does not match checkpoints length', () => {
+        const getDistance = () => distanceWithTime(path1, path2, [1638266548305, 1638266554087, 1638266564618, 1638266571176]);
+        expect(getDistance).to.throw(RangeError, 'Timestamps array length must match reference path locations count.');
+    });
 });
