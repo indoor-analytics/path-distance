ef09749720cc029664a9a043c202e64d9b18b3db
Rémy Raes <contact@remyraes.com>
2021-12-07 15:40:30 +0100

    [refactor] exporting both functions via package entrypoint

--- main.d.ts
+++ main.d.ts
@@ -1,2 +1,4 @@
-import { classicPathDistance } from "./src/classicDistance";
-export { classicPathDistance };
+import { ErrorVector } from "@indoor-analytics/entities";
+import { Feature, LineString } from "@turf/helpers";
+declare function pathDistance(referencePath: Feature<LineString>, comparedPath: Feature<LineString>, checkpointsTimestamps?: number[]): ErrorVector[];
+export { pathDistance };
--- main.ts
+++ main.ts
@@ -1,2 +1,16 @@
 import {classicPathDistance} from "./src/classicDistance";
-export {classicPathDistance};
+import {ErrorVector} from "@indoor-analytics/entities";
+import {Feature, LineString} from "@turf/helpers";
+import {distanceWithTime} from "./src/distanceWithTime";
+
+function pathDistance (
+    referencePath: Feature<LineString>,
+    comparedPath: Feature<LineString>,
+    checkpointsTimestamps: number[] = []
+): ErrorVector[] {
+    return checkpointsTimestamps.length !== 0 && comparedPath.properties !== null && comparedPath.properties.hasOwnProperty('locationsTimestamps')
+        ? distanceWithTime(referencePath, comparedPath as Feature<LineString, {locationsTimestamps: number[]}>, checkpointsTimestamps)
+        : classicPathDistance(referencePath, comparedPath);
+}
+
+export {pathDistance};
