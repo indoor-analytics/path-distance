f3520769579708ca95e71ec3d126b0787a2a9620
Rémy Raes <contact@remyraes.com>
2021-12-08 14:29:01 +0100

    [test] removing vectors lengths comparison test
    Time distance algorithm may exclude data points that have not been captured (or
    marked as having been acquired) in the checkpoints timestamps interval (= points
    received before first checkpoint or after last checkpoint).

--- test/comparison.test.ts
+++ test/comparison.test.ts
@@ -39,16 +39,6 @@ it ('should be more accurate than pathDistance method with run having many locat
     printCollectionToFile(featureCollection([referencePath, comparedPath, ...classicVectorsLines]), 'classicVectors');
 });
 
-it ('should return same vectors count', () => {
-    const referencePath = getRailwayReferenceWithDoubleStartingPoint();
-    const comparedPath = getTimedRun();
-
-    const classicVectors = pathDistance(referencePath, comparedPath);
-    const timeVectors = distanceWithTime(referencePath, comparedPath, getCheckpointsWithDoubleSecondTime());
-
-    expect(classicVectors.length).to.equal(timeVectors.length);
-});
-
 it ('should return vectors that end on same position', () => {
     const referencePath = getRailwayReferenceWithDoubleStartingPoint();
     const comparedPath = getTimedRun();
